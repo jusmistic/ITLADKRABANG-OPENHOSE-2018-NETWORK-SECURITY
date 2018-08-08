6bc62ff2d121cf6142eb0c1267d1b8ac486cb1d6
Jusmistic <jusmistic@gmail.com>
2018-08-08 20:16:03 +0700

    add init.sh

---
+++ web/init.sh
@@ -0,0 +1,30 @@
+#web-100 install 
+cd web-100-expore-the-web &&\
+docker build -t web-100-container . &&\
+docker run -dit --rm --name web-100 -p 8100:80 web-100-container &&
+cd .. &&
+
+#web-101 install 
+cd web-101-robot &&\
+docker build -t web-101-container . &&\
+docker run -dit --rm --name web-101 -p 8101:80 web-101-container &&
+cd .. &&
+
+#web-102 install
+cd web-102-Eltimate-robot &&\
+docker build -t web-102-container . &&\
+docker run -dit --rm --name web-102 -p 8102:80 web-102-container &&
+cd .. &&
+
+#web-201 install 
+cd web-201-CookieJar &&\
+docker build -t web-201-container . &&\
+docker run -dit --rm --name web-201 -p 8201:8201 web-201-container &&
+cd .. &&
+
+#web-202 install 
+cd web-202-debugger &&\
+docker build -t web-202-container . &&\
+docker run -it --rm --env-file .env --name web-202 -p 8202:80 web-202-container 
+
+echo "\n\n============[ Web Deploy Complete ]============\n\n"
