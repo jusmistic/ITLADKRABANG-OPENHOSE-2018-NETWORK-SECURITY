aab64c808a7320833d0a9c71f7b3468978350c1b
Jusmistic <jusmistic@gmail.com>
2018-08-09 11:13:07 +0700

    fix toLowerString bug when cookie is undifined

--- web/web-201-CookieJar/app/routes/index.js
+++ web/web-201-CookieJar/app/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  var cookie = req.cookies.user.toLowerCase();
+  var cookie = req.cookies.user;
+  if (cookie != undefined) cookie.toLowerCase();
   var visitor = '';
   
   if (cookie == 'yeri' || cookie == 'joy' || cookie == 'irene' || cookie == 'seulgi' || cookie == 'wendy' || cookie == 'cookie_jar')
